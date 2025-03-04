e4ce18d8c85e1ee4b17e31b583b90021607ff052
IouriMARTIN <iourimartin@gmail.com>
2025-03-04 10:18:42 +0100

    0.0.2 resolve conflicts

--- src/fochamon/Attack.java
+++ src/fochamon/Attack.java
@@ -1,6 +1,20 @@
 package fochamon;
 
 public abstract class Attack {
+	private String name;
+	private String category;
+	private int power;
+	private int accuracy;
 	private Type type;
+	
+	public Attack(String name, String category, int power, int accuracy, Type type) {
+		this.accuracy = accuracy;
+		this.category = category;
+		this.name = name;
+		this.power = power;
+		this.type = type;
+	}
+	
+	public abstract void effect();
 	
 }
