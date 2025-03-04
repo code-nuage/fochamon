5590e8e7c2f1c35aacd0c66c57121eb9b546a078
IouriMARTIN <iourimartin@gmail.com>
2025-03-04 11:42:51 +0100

    0.0.3 added contents for Attack

--- src/fochamon/Attack.java
+++ src/fochamon/Attack.java
@@ -1,13 +1,19 @@
 package fochamon;
 
 public abstract class Attack {
+	// +-- PROPERTIES --+
 	private String name;
 	private String category;
 	private int power;
 	private int accuracy;
 	private Type type;
 	
-	public Attack(String name, String category, int power, int accuracy, Type type) {
+	// +-- CONSTRUCTOR --+
+	public Attack(String name,
+			String category, 
+			int power, 
+			int accuracy, 
+			Type type) {
 		this.accuracy = accuracy;
 		this.category = category;
 		this.name = name;
@@ -15,6 +21,62 @@ public abstract class Attack {
 		this.type = type;
 	}
 	
-	public abstract void effect();
+	// +-- GETTERS --+
+	public String getName() {
+		return this.name;
+	}
 	
+	public String getCategory() {
+		return this.category;
+	}
+	
+	public int getPower() {
+		return this.power;
+	}
+	
+	public int getAccuracy() {
+		return this.accuracy;
+	}
+	
+	public Type getType() {
+		return this.type;
+	}
+	
+	// +-- SETTERS --+
+	public Attack setName(String name) {
+		this.name = name;
+		return this;
+	}
+	
+	public Attack setCategory(String category) {
+		this.category = category;
+		return this;
+	}
+	
+	public Attack setPower(int power) {
+		this.power = power;
+		return this;
+	}
+	
+	public Attack setAccuracy(int accuracy) {
+		this.accuracy = accuracy;
+		return this;
+	}
+	
+	public Attack setType(Type type) {
+		this.type = type;
+		return this;
+	}
+	
+	// +-- STRINGIFIERS --+
+	public String toString() {
+		return this.name + "\n" +
+			   "Category: " + this.category + "\n" +
+			   "Power: " + this.power + "\n" +
+			   "Accuracy: " + this.accuracy + "\n" +
+			   this.type.getName();
+		} 
+	
+	// +-- ALGORYTHM --+
+	public abstract void effect();
 }
