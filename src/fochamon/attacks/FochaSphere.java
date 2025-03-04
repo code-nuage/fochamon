049154b12311a29caaea1b992b415f31bf6dda16
IouriMARTIN <iourimartin@gmail.com>
2025-03-04 13:56:24 +0100

    0.0.5 added attack folder

---
+++ src/fochamon/attacks/FochaSphere.java
@@ -0,0 +1,12 @@
+package fochamon.attacks;
+import fochamon.Attack;
+import fochamon.Types.Fire;
+
+public class FochaSphere extends Attack{
+	public FochaSphere() {
+		super("Focha-sphère", "physique", 60, 100, new Fire());
+	}
+
+	public void effect() {
+	}
+}
